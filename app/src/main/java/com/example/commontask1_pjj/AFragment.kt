dc21eaaeee8516af32142ae75375016ba6e9afcf
Jongjunp <56390530+Jongjunp@users.noreply.github.com>
2021-12-29 12:57:19 +0900

    project1_jongjun - ongoing for implementing recycler

--- app/src/main/java/com/example/commontask1_pjj/AFragment.kt
+++ app/src/main/java/com/example/commontask1_pjj/AFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 class AFragment : Fragment() {
+
+    val item_list : ArrayList<ItemData> = ArrayList()
+    lateinit var recyclerView1 : RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false)
+        var rootView = inflater.inflate(R.layout.fragment_a, container, false)
+        item_list.add(ItemData("PJJ",22))
+
+        recyclerView1 = rootView.findViewById(R.id.)
+
     }
 }
---
+++ app/src/main/java/com/example/commontask1_pjj/ItemData.kt
@@ -0,0 +1,6 @@
+package com.example.commontask1_pjj
+
+data class ItemData (
+    val name : String
+    val age: Int
+        )

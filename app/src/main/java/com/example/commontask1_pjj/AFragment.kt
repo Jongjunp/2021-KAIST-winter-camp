beac6bc97ef0a5ffd33e6078d148925aa49e09c3
jjminsuh <minsuh3203@gmail.com>
2021-12-29 12:51:16 +0900

    project1_minsuh

--- app/src/main/java/com/example/commontask1_pjj/AFragment.kt
+++ app/src/main/java/com/example/commontask1_pjj/AFragment.kt
@@ -5,14 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 class AFragment : Fragment() {
+
+    private lateinit var rv: RecyclerView
+    private lateinit var my_adapter: ViewAdapter
+    private val repository = innerData()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        my_adapter = ViewAdapter(this)
+        rv.adapter = my_adapter
+        my_adapter = repository.getRepoList()
+
     }
 }

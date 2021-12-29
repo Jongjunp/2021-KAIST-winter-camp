aeb68512d43402d9a1e04b9efcc0ffc6a795f061
jjminsuh <minsuh3203@gmail.com>
2021-12-29 15:55:40 +0900

    project1_minsuh - recyclerview list, no data

--- app/src/main/java/com/example/commontask1_pjj/AFragment.kt
+++ app/src/main/java/com/example/commontask1_pjj/AFragment.kt
@@ -5,12 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class AFragment : Fragment() {
+    val dataArray = ArrayList<ListItem>()
+    lateinit var recyclerView1: RecyclerView
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var rootView = inflater.inflate(R.layout.fragment_a, container, false)
+        dataArray.add(ListItem("홍길동", "010-1234-5678"))
+        dataArray.add(ListItem("이름", "010-0987-6543"))
+
+        recyclerView1 = rootView.findViewById(R.id.recyclerView)as RecyclerView
+        recyclerView1.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView1.adapter = ViewAdapter(requireContext(), dataArray)
+
+        return rootView
+    }
+    /*
     private lateinit var rv: RecyclerView
-    private lateinit var my_adapter: ViewAdapter
+    private lateinit var myAdapter: ViewAdapter
     private val repository = innerData()
 
     override fun onCreateView(
@@ -20,11 +39,12 @@ class AFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_a, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        my_adapter = ViewAdapter(this)
-        rv.adapter = my_adapter
-        my_adapter.data = repository.getRepoList()
+        myAdapter = ViewAdapter(this)
+        //rv.adapter = myAdapter
+        myAdapter.data = repository.getRepoList()
 
-    }
+    }*/
 }

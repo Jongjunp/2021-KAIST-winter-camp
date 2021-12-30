567484120305637427b124d6e70d906bde457e73
jjminsuh <minsuh3203@gmail.com>
2021-12-30 21:33:51 +0900

    project1_minsuh - tab3 onclick activity intent finish

--- app/src/main/java/com/example/commontask1_pjj/CFragment.kt
+++ app/src/main/java/com/example/commontask1_pjj/CFragment.kt
@@ -1,11 +1,13 @@
 package com.example.commontask1_pjj
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -38,7 +40,10 @@ class CFragment : Fragment() {
         recyclerView1.adapter = adapter
         adapter.setOnItemClickListener(object: ViewAdapterC.onItemClickListener{
             override fun onItemClick(position: Int) {
-                Toast.makeText(context, "test", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(context, "test", Toast.LENGTH_SHORT).show()
+                val intent = Intent(activity, GenreActivity::class.java)
+                activity?.startActivity(intent)
+                //putextra로 data 전달
             }
         })
         recyclerView1.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))

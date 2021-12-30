f1a911e08153b3327f70e4f93225a2a94eacf93b
jjminsuh <minsuh3203@gmail.com>
2021-12-30 10:56:59 +0900

    project1_minsuh - recyclerview with random phone data

--- app/src/main/java/com/example/commontask1_pjj/AFragment.kt
+++ app/src/main/java/com/example/commontask1_pjj/AFragment.kt
@@ -1,5 +1,6 @@
 package com.example.commontask1_pjj
 
+import android.content.res.AssetManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONArray
+import org.json.JSONObject
 
 class AFragment : Fragment() {
     private val dataArray = ArrayList<ListItem>()
@@ -19,13 +22,23 @@ class AFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var rootView = inflater.inflate(R.layout.fragment_a, container, false)
-        dataArray.add(ListItem("홍길동", "010-1234-5678"))
-        dataArray.add(ListItem("이름", "010-0987-6543"))
-        dataArray.add(ListItem("이름", "010-0987-6543"))
-        dataArray.add(ListItem("이름", "010-0987-6543"))
-        dataArray.add(ListItem("이름", "010-0987-6543"))
-        dataArray.add(ListItem("이름", "010-0987-6543"))
-        dataArray.add(ListItem("이름", "010-0987-6543"))
+
+        val assetManager = resources.assets
+        val inputStream = assetManager.open("phoneData.json")
+        val jsonString = inputStream.bufferedReader().use {it.readText()}
+
+        val jObject = JSONObject(jsonString)
+        val jArray = jObject.getJSONArray("phoneData")
+
+        for(i in 0 until jArray.length()){
+
+            val obj = jArray.getJSONObject(i)
+
+            val jName = obj.getString("name")
+            val jPhone = obj.getString("phone")
+
+            dataArray.add(ListItem(jName, jPhone))
+        }
 
         recyclerView1 = rootView.findViewById(R.id.recyclerView)as RecyclerView
         recyclerView1.layoutManager = LinearLayoutManager(requireContext())
@@ -34,24 +47,5 @@ class AFragment : Fragment() {
 
         return rootView
     }
-    /*
-    private lateinit var rv: RecyclerView
-    private lateinit var myAdapter: ViewAdapter
-    private val repository = innerData()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_a, container, false)
-    }
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        myAdapter = ViewAdapter(this)
-        //rv.adapter = myAdapter
-        myAdapter.data = repository.getRepoList()
-
-    }*/
 }
--- app/src/main/java/com/example/commontask1_pjj/ListItem.kt
+++ app/src/main/java/com/example/commontask1_pjj/ListItem.kt
@@ -1,5 +1,7 @@
 package com.example.commontask1_pjj
 
+import android.graphics.drawable.Drawable
+
 //각 item에 필요한 data
 data class ListItem(
     val name: String,

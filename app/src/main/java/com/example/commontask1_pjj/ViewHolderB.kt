4e6328990cd125bd4620446e9ab682956d55dc7c
Jongjunp <56390530+Jongjunp@users.noreply.github.com>
2022-01-01 20:46:09 +0900

    project1_jongjun: tab2 temp2

--- app/src/main/java/com/example/commontask1_pjj/ViewHolderB.kt
+++ app/src/main/java/com/example/commontask1_pjj/ViewHolderB.kt
@@ -1,22 +1,30 @@
 package com.example.commontask1_pjj
 
+import android.content.Intent
 import android.content.res.Resources
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.util.Base64
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.recyclerview.widget.RecyclerView
+import org.jetbrains.anko.find
 
 //뷰의 각 요소 관리
 class ViewHolderB(view: View) : RecyclerView.ViewHolder(view) {
-    var v_photo: ImageView=view.findViewById(R.id.item_photo)
+    var photoButton: ImageButton = view.findViewById(R.id.item_photo)
+    var titleText: TextView = view.findViewById(R.id.item_title)
 
     //data setting
-    fun bind(dataArray: dataVo){
-        val imageBytes = Base64.decode(dataArray.photo,0)
-        val bitMapImage = BitmapFactory.decodeByteArray(imageBytes,0,imageBytes.size)
-        v_photo.setImageBitmap(bitMapImage)
+    fun bind(dataArray: dataVo) {
+        val imageBytes = Base64.decode(dataArray.photo, 0)
+        val titleBytes = dataArray.title
+        val bitMapImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+        photoButton.setImageBitmap(bitMapImage)
+        titleText.setText(titleBytes)
     }
+
 }

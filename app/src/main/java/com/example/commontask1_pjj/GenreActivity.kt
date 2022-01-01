2b48ee52660463df9ed0abe10d3a0f8e9e9dbe5d
jjminsuh <minsuh3203@gmail.com>
2022-01-01 15:01:47 +0900

    project1_minsuh - tab3 graph design

--- app/src/main/java/com/example/commontask1_pjj/GenreActivity.kt
+++ app/src/main/java/com/example/commontask1_pjj/GenreActivity.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Description
@@ -36,27 +37,38 @@ class GenreActivity : AppCompatActivity() {
 
     private fun initPieChart(){
         pieChart.setUsePercentValues(false)
-        pieChart.description.isEnabled = false
-        //pieChart.description.text = "별점 분포"
-        //pieChart.description.textSize = 16f
 
-        pieChart.legend.textSize = 20f
+        pieChart.description.isEnabled = false
+        //pieChart.description.textSize = 16f
+        //pieChart.centerText = "Ratings"
+        //pieChart.setCenterTextSize(20f)
+
+        pieChart.setDrawEntryLabels(true)
+        pieChart.setEntryLabelColor(resources.getColor(R.color.black))
+        pieChart.setEntryLabelTextSize(20f)
+
+        //pieChart.holeRadius = 30f
+        //pieChart.transparentCircleRadius = 40f
+
+        pieChart.setExtraOffsets(25f, 0f, 25f, 0f)
+
+        //pieChart.legend.textSize = 20f
+        pieChart.legend.isEnabled = false
     }
 
     private fun setPieChart(){
 
         val colors: ArrayList<Int> = ArrayList()
         colors.add(Color.parseColor("#FF0000"))
-        colors.add(Color.parseColor("#FFFF00"))
+        colors.add(Color.parseColor("#FFBF00"))
         colors.add(Color.parseColor("#00FF00"))
         colors.add(Color.parseColor("#0000FF"))
         colors.add(Color.parseColor("#8000FF"))
 
-        val xVal = Array<Int>(5) { _ -> 0}
+        val yVal = Array<Int>(5) { _ -> 0}
 
         val genreName = findViewById<View>(R.id.item_genre) as TextView
-        genreName.text = intent.getStringExtra("Genre")
-        val count = 1
+        genreName.text = intent.getStringExtra("Genre") + "영화 별점분포"
 
         val appSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.applicationContext)
         val gson = Gson()
@@ -69,22 +81,31 @@ class GenreActivity : AppCompatActivity() {
             var obj = gson.fromJson(json, ReviewData::class.java)
 
             if(obj.movieGenre == intent.getStringExtra("Genre")){
-                xVal[(obj.movieRating?.toInt() ?: 1) -1] = xVal[(obj.movieRating?.toInt() ?: 1) -1]?.plus(1)
+                yVal[(obj.movieRating?.toInt() ?: 1) -1] = yVal[(obj.movieRating?.toInt() ?: 1) -1]?.plus(1)
             }
         }
 
         //data
         val entries = ArrayList<PieEntry>()
-        entries.add(PieEntry(xVal[0].toFloat(), "1"))
-        entries.add(PieEntry(xVal[1].toFloat(), "2"))
-        entries.add(PieEntry(xVal[2].toFloat(), "3"))
-        entries.add(PieEntry(xVal[3].toFloat(), "4"))
-        entries.add(PieEntry(xVal[4].toFloat(), "5"))
 
-        val pieDataSet = PieDataSet(entries, "별점")
+        for(i: Int in 0..4)
+        {
+            if(yVal[i]!= 0)
+            {
+                entries.add(PieEntry(yVal[i].toFloat(), (i+1).toString() + "점"))
+            }
+        }
+
+
+        val pieDataSet = PieDataSet(entries, "")
 
         pieDataSet.colors = colors
         pieDataSet.sliceSpace = 5f
+        pieDataSet.valueLinePart1Length = 0.6f
+        pieDataSet.valueLinePart2Length = 0.3f
+        pieDataSet.valueLineWidth = 2f
+        pieDataSet.isUsingSliceColorAsValueLineColor = true
+        pieDataSet.xValuePosition = PieDataSet.ValuePosition.OUTSIDE_SLICE
 
         val data = PieData(pieDataSet)
         pieChart.data = data
@@ -92,7 +113,7 @@ class GenreActivity : AppCompatActivity() {
 
         pieChart.setBackgroundColor(resources.getColor(R.color.white))
 
-        pieChart.animateY(1000)
+        pieChart.animateY(1000,Easing.EaseInOutQuad)
 
         pieChart.invalidate()
     }
